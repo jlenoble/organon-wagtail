6fa24614b7299a5b2afb110c02629a694dd2f708
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-05-28 22:57:43 +0200

    👷 build(env): initial setup
    python3 -m venv .venv
    python3 -m pip install wagtail
    wagtail start organon_wagtail .
    python3 -m pip install -r requirements.txt
    python3 manage.py migrate
    python3 manage.py createsuperuser
    python3 manage.py runserver

---
+++ organon_wagtail/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-a+$$g%mchpxd9-(!&bph-4pl5xa2%$hy0mq33m58_^ol(5zi)-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
