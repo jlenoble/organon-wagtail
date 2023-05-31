a0a06fb7b131eeada6e3be376a083fd61850ecc8
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-05-31 23:56:15 +0200

    ✨ feat(home): banner_title field

--- home/models.py
+++ home/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    template = "home/home_page.html"
+
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+
+    content_panels = Page.content_panels + [FieldPanel("banner_title")]
