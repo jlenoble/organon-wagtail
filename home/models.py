94a6783fa8eb9bd224dd2638cd05f41b01417350
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-06-01 16:13:15 +0200

    ✨ feat(home): home page is unique

--- home/models.py
+++ home/models.py
@@ -6,6 +6,7 @@ from wagtail.admin.panels import FieldPanel
 
 class HomePage(Page):
     template = "home/home_page.html"
+    max_count = 1
 
     banner_title = models.CharField(max_length=100, blank=False, null=True)
 
