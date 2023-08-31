f4f7ef5355b6020e277f3c4406718d843a9f0432
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-08-31 15:58:18 +0200

    ✨ feat(blog): add body streamfield
    May contain titles, rich texts and images in any order.

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,9 @@
 from wagtail.models import Page
+from wagtail import blocks
+from wagtail.fields import StreamField
+from wagtail.images.blocks import ImageChooserBlock
+
+from wagtail.admin.panels import FieldPanel
 
 
 class BlogIndexPage(Page):
@@ -8,3 +13,21 @@ class BlogIndexPage(Page):
 
 class BlogPostPage(Page):
     parent_page_types = ["blog.BlogIndexPage"]
+
+    body = StreamField(
+        [
+            (
+                "title",
+                blocks.CharBlock(
+                    form_classname="full title", template="blocks/title_block.html"
+                ),
+            ),
+            ("richtext", blocks.RichTextBlock()),
+            ("image", ImageChooserBlock()),
+        ],
+        use_json_field=True,
+        blank=True,
+        null=True,
+    )
+
+    content_panels = Page.content_panels + [FieldPanel("body")]
